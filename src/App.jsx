3d6d5b3258c189038822965a5be2bd626dd3e89b
Olena Zdanovska <freshjs@gmail.com>
2023-09-19 12:02:02 +0300

    Refactor App

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import { UserContext, ThemeContext } from './contexts';
@@ -19,62 +19,49 @@ import FormsPage from './pages/FormsPage';
 import { THEMES } from './constants';
 import CallbackForm from './components/forms/CallbackForm';
 
+const App = (props) => {
+  const [user, setUser] = useState({
+    id: 1,
+    firstName: 'Brad',
+    lastName: 'Pitt',
+    isSelect: false,
+    avatar: 'https://cdn-icons-png.flaticon.com/128/3641/3641963.png',
+  });
+  const [theme, setTheme] = useState(THEMES.LIGHT);
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      user: {
-        id: 1,
-        firstName: 'Brad',
-        lastName: 'Pitt',
-        isSelect: false,
-        avatar: 'https://cdn-icons-png.flaticon.com/128/3641/3641963.png',
-      },
-      theme: THEMES.LIGHT,
-    };
-  }
-
-  changeTheme = () => {
-    this.setState({
-      theme: this.state.theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT,
-    });
+  const changeTheme = () => {
+    setTheme(theme === THEMES.LIGHT ? THEMES.DARK : THEMES.LIGHT);
   };
 
-  selectorUser = (id) => {
-    this.setState({
-      user: { ...this.state.user, isSelect: !this.state.user.isSelect },
-    });
+  const selectorUser = (id) => {
+    setUser({ ...user, isSelect: !user.isSelect });
   };
 
-  render() {
-    const { user, theme } = this.state;
-    return (
-      <ThemeContext.Provider value={[theme, this.changeTheme]}>
-        <UserContext.Provider value={{ user, selectorUser: this.selectorUser }}>
-          <BrowserRouter>
-            <Header />
-            <main>
-              <Routes>
-                <Route path="/" element={<Home />} />
-                <Route path="/sign-up" element={<FormsPage />} />
-                <Route path="/sign-in" element={<SignInForm />} />
-                <Route path="/callback" element={<CallbackForm />} />
-                <Route path="/users" element={<UsersPage />} />
-                <Route path="/loader/" element={<LoaderPage />}>
-                  <Route path="users" element={<UsersBlock />} />
-                  <Route path="events" element={<EventsBlock />} />
-                  <Route path="products" element={<ProductsBlock />} />
-                </Route>
-                <Route path="*" element={<Page404 />} />
-              </Routes>
-            </main>
-            <Footer />
-          </BrowserRouter>
-        </UserContext.Provider>
-      </ThemeContext.Provider>
-    );
-  }
-}
+  return (
+    <ThemeContext.Provider value={[theme, changeTheme]}>
+      <UserContext.Provider value={{ user, selectorUser: selectorUser }}>
+        <BrowserRouter>
+          <Header />
+          <main>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/sign-up" element={<FormsPage />} />
+              <Route path="/sign-in" element={<SignInForm />} />
+              <Route path="/callback" element={<CallbackForm />} />
+              <Route path="/users" element={<UsersPage />} />
+              <Route path="/loader/" element={<LoaderPage />}>
+                <Route path="users" element={<UsersBlock />} />
+                <Route path="events" element={<EventsBlock />} />
+                <Route path="products" element={<ProductsBlock />} />
+              </Route>
+              <Route path="*" element={<Page404 />} />
+            </Routes>
+          </main>
+          <Footer />
+        </BrowserRouter>
+      </UserContext.Provider>
+    </ThemeContext.Provider>
+  );
+};
 
 export default App;
