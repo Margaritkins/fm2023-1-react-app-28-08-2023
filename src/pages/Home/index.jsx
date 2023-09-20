fb1a791b7bdf2cb0acba5c85863b9b19de3bead1
Olena Zdanovska <freshjs@gmail.com>
2023-09-20 09:34:15 +0300

    Example inlineStyle

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,23 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import CardUser from '../../components/CardSection/CardUser';
 import LearnHooks from '../../components/LearnHooks';
 import StopWatchHooks from '../../components/StopWatchHooks';
+import { ThemeContext } from '../../contexts';
+import { THEMES } from '../../constants';
+
+const styleMap = {
+  [THEMES.LIGHT]:{backgroundColor:'#eee', color: '#222'},
+  [THEMES.DARK]:{backgroundColor:'#222', color: '#eee'},
+}
 
 const Home = () => {
+  const [theme] = useContext(ThemeContext);
   return (
-    <div>
+    <div style={styleMap[theme]}>
       <StopWatchHooks />
       <LearnHooks />
       <h1>'Home':'Домашня'</h1>
-      <CardUser />     
+      <CardUser />
     </div>
   );
 };
