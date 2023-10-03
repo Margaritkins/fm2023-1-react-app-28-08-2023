ad24efb127ca97acd279a19a371a50d514b7d3af
Margarita <te120883ama@gmail.com>
2023-10-03 23:53:54 +0200

    Add change theme in Footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,11 +1,21 @@
-import React from 'react';
+import React from "react";
+import { ThemeContext } from "../../contexts";
+import cx from "classnames";
+import styles from "./Footer.module.scss";
+import { THEMES } from "../../constants";
+
+
 
 const Footer = () => {
-  return (
-    <div>
-      &copy;2023
-    </div>
-  );
-}
+  const renderFunc = ([theme]) => {
+    const classes = cx(styles.container, {
+      [styles.light]: theme === THEMES.LIGHT,
+      [styles.dark]: theme === THEMES.DARK,
+    });
+    return <div className={classes}>&copy;2023</div>
+  };
+
+  return <ThemeContext.Consumer>{renderFunc}</ThemeContext.Consumer>;
+};
 
 export default Footer;
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,18 @@
 import React from "react";
 import { ThemeContext } from "../../contexts";
-import cx from 'classnames';
+import cx from "classnames";
 import NavMenu from "../NavMenu";
 import Avatar from "../Avatar";
 import SwitchTheme from "../SwitchTheme";
 import styles from "./Header.module.scss";
-import {THEMES} from '../../constants'
+import { THEMES } from "../../constants";
 
 const Header = () => {
   const renderFunc = ([theme]) => {
     const classes = cx(styles.container, {
-      [styles.light]:theme===THEMES.LIGHT,
-      [styles.dark]:theme===THEMES.DARK
-    })
+      [styles.light]: theme === THEMES.LIGHT,
+      [styles.dark]: theme === THEMES.DARK,
+    });
     return (
       <div className={classes}>
         <NavMenu />
@@ -22,13 +22,7 @@ const Header = () => {
     );
   };
   return <ThemeContext.Consumer>{renderFunc}</ThemeContext.Consumer>;
-  // return (
-  //   <div style={{display:'flex',justifyContent:'space-between'}}>
-  //     <NavMenu />
-  //     <Avatar />
-  //     <SwitchTheme />
-  //   </div>
-  // );
+
 };
 
 export default Header;
